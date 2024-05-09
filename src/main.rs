985a8d445897562e0367c847c3b804c223b9f4bc
JeroenLam <70508825+JeroenLam@users.noreply.github.com>
2024-05-09 08:48:31 +0200

    Added hello world example to app

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_systems(Update, hello_world_system)
+        .run();
 }
+
+fn hello_world_system() {
+    println!("hello world");
+ }
