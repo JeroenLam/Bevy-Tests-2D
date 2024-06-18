e13d619c4ce3917d463155b0a6a0778a8934228b
JeroenLam <70508825+JeroenLam@users.noreply.github.com>
2024-06-19 00:03:11 +0200

    Added platform generation based on high level location, size and material parameters

--- src/map_platforms.rs
+++ src/map_platforms.rs
@@ -11,20 +11,127 @@ impl Plugin for MapPlatformsPlugin {
     }
 }
 
-fn spawn_map_platforms(mut commands: Commands) {
+fn spawn_map_platforms(mut commands: Commands, terrain_assets: ResMut<TerrainAssets>) {
     // Ground
-    commands.spawn((
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::rgb(0.3, 0.3, 0.7),
-                custom_size: Some(Vec2::new(800.0, 20.0)),
-                ..Default::default()
+    spawn_map_platform(
+        &mut commands,
+        TerrainObject::new(0., -48., 20, 2, TerrainType::GrassGreen),
+        &terrain_assets,
+    );
+
+    spawn_map_platform(
+        &mut commands,
+        TerrainObject::new(200., 0., 3, 3, TerrainType::Plant),
+        &terrain_assets,
+    );
+
+    spawn_map_platform(
+        &mut commands,
+        TerrainObject::new(-200., 0., 4, 4, TerrainType::GrassOrange),
+        &terrain_assets,
+    );
+}
+
+#[derive(Debug)]
+pub struct TerrainObject {
+    translation_x: f32,
+    translation_y: f32,
+    size_x: i32,
+    size_y: i32,
+    terrain_type: TerrainType,
+}
+
+impl TerrainObject {
+    pub fn new(
+        translation_x: f32,
+        translation_y: f32,
+        size_x: i32,
+        size_y: i32,
+        terrain_type: TerrainType,
+    ) -> Self {
+        Self {
+            translation_x,
+            translation_y,
+            size_x,
+            size_y,
+            terrain_type,
+        }
+    }
+}
+
+fn spawn_map_platform(
+    commands: &mut Commands,
+    terrain_object: TerrainObject,
+    terrain_assets: &ResMut<TerrainAssets>,
+    // materials: &mut ResMut<Assets<ColorMaterial>>,
+) {
+    if terrain_object.size_x < 2 || terrain_object.size_y < 2 {
+        todo!()
+    }
+    // Create the parent entity
+    let parent = commands
+        .spawn((
+            RigidBody::Fixed,
+            Collider::cuboid(
+                (terrain_object.size_x * 8) as f32,
+                (terrain_object.size_y * 8) as f32,
+            ),
+            SpatialBundle {
+                transform: Transform::from_translation(Vec3::new(
+                    terrain_object.translation_x,
+                    terrain_object.translation_y,
+                    0.0,
+                )),
+                visibility: Visibility::Inherited,
+                ..default()
             },
-            ..Default::default()
-        },
-        RigidBody::Fixed,
-        Collider::cuboid(480.0, 8.0),
-    ));
+        ))
+        .id();
+
+    let atlas_layout = terrain_assets
+        .map
+        .get(&terrain_object.terrain_type)
+        .unwrap();
+    let texture = &terrain_assets.image;
+
+    // Spawn children to properly render the block
+    for y in 0..terrain_object.size_y {
+        for x in 0..terrain_object.size_x {
+            let mut idx;
+            if x == terrain_object.size_x - 1 {
+                idx = 2;
+            } else if x == 0 {
+                idx = 0;
+            } else {
+                idx = 1;
+            }
+
+            if y == terrain_object.size_y - 1 {
+                idx += 0;
+            } else if y == 0 {
+                idx += 6;
+            } else {
+                idx += 3;
+            }
+
+            // TODO: Implement this correctly
+            let trans_x = terrain_object.translation_x + (x * 16) as f32;
+            let trans_y = terrain_object.translation_y + (y * 16) as f32;
+
+            let child = commands
+                .spawn((SpriteSheetBundle {
+                    texture: texture.clone().unwrap(),
+                    atlas: TextureAtlas {
+                        layout: atlas_layout.clone(),
+                        index: idx,
+                    },
+                    transform: Transform::from_translation(Vec3::new(trans_x, trans_y, 0.0)),
+                    ..default()
+                },))
+                .id();
+            commands.entity(parent).push_children(&[child]);
+        }
+    }
 }
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
@@ -60,21 +167,21 @@ fn load_terain_assets(
 
     // Load Wood texture
     let atlas_layout =
-        TextureAtlasLayout::from_grid(Vec2::splat(16.), 3, 3, None, Some(Vec2::new(0., 4.)));
+        TextureAtlasLayout::from_grid(Vec2::splat(16.), 3, 3, None, Some(Vec2::new(0., 64.)));
     terrain_assets
         .map
         .insert(TerrainType::Wood, texture_atlas_layouts.add(atlas_layout));
 
     // Load Plant texture
     let atlas_layout =
-        TextureAtlasLayout::from_grid(Vec2::splat(16.), 3, 3, None, Some(Vec2::new(0., 7.)));
+        TextureAtlasLayout::from_grid(Vec2::splat(16.), 3, 3, None, Some(Vec2::new(0., 128.)));
     terrain_assets
         .map
         .insert(TerrainType::Plant, texture_atlas_layouts.add(atlas_layout));
 
     // Load Grass Green texture
     let atlas_layout =
-        TextureAtlasLayout::from_grid(Vec2::splat(16.), 3, 3, None, Some(Vec2::new(6., 0.)));
+        TextureAtlasLayout::from_grid(Vec2::splat(16.), 3, 3, None, Some(Vec2::new(96., 0.)));
     terrain_assets.map.insert(
         TerrainType::GrassGreen,
         texture_atlas_layouts.add(atlas_layout),
@@ -82,7 +189,7 @@ fn load_terain_assets(
 
     // Load GrassOrange texture
     let atlas_layout =
-        TextureAtlasLayout::from_grid(Vec2::splat(16.), 3, 3, None, Some(Vec2::new(6., 4.)));
+        TextureAtlasLayout::from_grid(Vec2::splat(16.), 3, 3, None, Some(Vec2::new(96., 64.)));
     terrain_assets.map.insert(
         TerrainType::GrassOrange,
         texture_atlas_layouts.add(atlas_layout),
@@ -90,7 +197,7 @@ fn load_terain_assets(
 
     // Load GrassPink texture
     let atlas_layout =
-        TextureAtlasLayout::from_grid(Vec2::splat(16.), 3, 3, None, Some(Vec2::new(6., 7.)));
+        TextureAtlasLayout::from_grid(Vec2::splat(16.), 3, 3, None, Some(Vec2::new(96., 128.)));
     terrain_assets.map.insert(
         TerrainType::GrassPink,
         texture_atlas_layouts.add(atlas_layout),
